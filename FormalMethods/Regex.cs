5c8bef6f0feaa816bb8b8df27599a214c5760902
Joëlle <joelle.a.m.devries@gmail.com>
2021-05-21 10:21:39 +0200

    Not done
    Hoe moeten de uitzonderings regels correct vewerkt worden. Is dit de juiste methode?

---
+++ FormalMethods/Regex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormalMethods
+{
+    class Regex
+    {
+        public string letters;
+        public string expression; 
+
+        public void FillLetters(string letters)
+        {
+            this.letters = letters; 
+        }
+
+        public void addLetter(string letter)
+        {
+            this.letters += letter; 
+        }
+
+        public void SetExpression(string expression)
+        {
+            this.expression = expression; 
+        }
+
+
+
+     }
+}
---
+++ FormalMethods/RegexParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormalMethods
+{
+    class RegexParser
+    {
+        public static void ParseRegex(string regex)
+        {
+            foreach(char c in regex.ToCharArray())
+            {
+                switch (c)
+                {
+                    case '(': //ignore
+                        break;
+                    case ')': //Last
+
+                        break;
+                    case '|': //or 
+
+                        break;
+                    case '+': //+ 
+    
+                        break;
+                    case '*': //*
+ 
+                        break;
+                    case '.': //.
+        
+                        break;
+                    default: //no special character detected
+                        break;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
