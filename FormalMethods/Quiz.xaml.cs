a3b1be4d188eb8531bf8b16c1dc4c70079022312
Phunderr <Phunderr>
2021-06-09 16:18:13 +0200

    [added] UI  for the quiz

---
+++ FormalMethods/Quiz.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace FormalMethods
+{
+    /// <summary>
+    /// Interaction logic for Quiz.xaml
+    /// </summary>
+    public partial class Quiz : Window
+    {
+
+        private bool CorrectAnswer;
+
+        public Quiz()
+        {
+            InitializeComponent();
+
+
+            //setup dispatch timer
+            DispatcherTimer time = new DispatcherTimer();
+            time.Tick += new EventHandler(Timer_tick);
+            time.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            time.Start();
+
+        }
+
+        public void Timer_tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
