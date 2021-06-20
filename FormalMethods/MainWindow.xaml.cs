e1bdcb689a5fdb9d216af6f5449f997b7bddbe8e
Phunderr <Phunderr>
2021-06-20 16:06:46 +0200

    {FIXED}

--- FormalMethods/MainWindow.xaml.cs
+++ FormalMethods/MainWindow.xaml.cs
@@ -53,53 +53,11 @@ namespace FormalMethods
             char[] alphabet = { 'a', 'b' };
             Automata<string> automata = new Automata<string>(alphabet);
 
-            automata.AddTransition(new Transition<string>("0", 'a', "1"));
-            automata.AddTransition(new Transition<string>("0", 'b', "4"));
-
-            automata.AddTransition(new Transition<string>("1", 'a', "4"));
-            automata.AddTransition(new Transition<string>("1", 'b', "2"));
-
-            automata.AddTransition(new Transition<string>("2", 'a', "4"));
-            automata.AddTransition(new Transition<string>("2", 'b', "3"));
-
-            automata.AddTransition(new Transition<string>("3", 'a', "3"));
-            automata.AddTransition(new Transition<string>("3", 'b', "3"));
-
-            automata.AddTransition(new Transition<string>("4", 'a', "4"));
-            automata.AddTransition(new Transition<string>("4", 'b', "5"));
-
-            automata.AddTransition(new Transition<string>("5", 'a', "6"));
-            automata.AddTransition(new Transition<string>("5", 'b', "5"));
-
-            automata.AddTransition(new Transition<string>("6", 'a', "7"));
-            automata.AddTransition(new Transition<string>("6", 'b', "5"));
-
-            automata.AddTransition(new Transition<string>("7", 'a', "4"));
-            automata.AddTransition(new Transition<string>("7", 'b', "8"));
-
-            automata.AddTransition(new Transition<string>("8", 'a', "4"));
-            automata.AddTransition(new Transition<string>("8", 'b', "5"));
-
-            automata.DefineAsStartState("0");
-
-            automata.DefineAsFinalState("3");
-            automata.DefineAsFinalState("8");
-
 
             List<string> valid = new List<string>();
             List<string> invalid = new List<string>();
 
-            foreach (string test in lijst) 
-            {
-                if (automata.Accept(test)) 
-                {
-                    valid.Add(test);
-                }
-                else 
-                {
-                    invalid.Add(test);
-                }
-            }
+            
 
             
 
